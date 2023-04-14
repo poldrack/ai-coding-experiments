abbb2afc7b4b1db7705c3e5b7906901599484726
Russ Poldrack <poldrack@gmail.com>
2023-04-14 12:53:21 -0700

    new header, parse args

--- generate_code_prompts.py
+++ generate_code_prompts.py
@@ -4,10 +4,21 @@ import os
 import pandas as pd
 import hashlib
 import json
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset', default='github')
+    parser.add_argument('-o', '--outdir', default='github_code_for_recoding')
+    return parser.parse_args()
 
 if __name__ == '__main__':
+    args = parse_args()
+    print(args)
+
+    infile = f'analysis_outputs/code_info_{args.dataset}.csv'
     # read in the csv file
-    df = pd.read_csv('code_info_github.csv', index_col=0)
+    df = pd.read_csv(infile, index_col=0)
     df['github_license'] = df['github_license'].fillna('None')
     print(f'number of code files: {len(df)}')
     df = df.dropna()
@@ -20,8 +31,12 @@ if __name__ == '__main__':
     for idx in df.index:
         filename = os.path.join('github_code',
                                 df.loc[idx, 'filename'])
-        hash = hashlib.sha256(filename.encode('utf-8')).hexdigest()
-        header = f'please recode the following Python code.  Please return the code within an explicit code cell.\n'
+    
+        hash = hashlib.sha256(filename.encode('utf-8')).hexdigest()[:20]
+        assert hash not in codeinfo
+    
+        # prompt1: header = f'please recode the following Python code.  Please return the code within an explicit code cell.\n'
+        header = "Please refactor the following Python code to be more readable, adding or rewriting comments as needed. Please embed the code within an explicit code block, surrounded by triple-backtick markers."
         header += f'# {hash}\n'
         with open(filename) as f:
             code = header + f.read()
@@ -29,8 +44,8 @@ if __name__ == '__main__':
             'filename': filename,
             'code': code,
         }
-        with open(f'github_code_for_recoding/{hash}.txt', 'w') as f:
+        with open(f'{args.outdir}/{hash}.txt', 'w') as f:
             f.write(code)
         
-        with open('github_code_for_recoding_info.json', 'w') as f:
+        with open(f'{args.outdir}.json', 'w') as f:
             json.dump(codeinfo, f)
