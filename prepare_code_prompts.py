5d434ca5895e06f5581e41b2b4de63d9c7e33bdb
Russ Poldrack <poldrack@gmail.com>
2023-04-25 08:10:18 -0700

    add v2 prompt

--- prepare_code_prompts.py
+++ prepare_code_prompts.py
@@ -3,10 +3,13 @@
 import os
 
 
-with open('data/coding_prompts.txt', 'r') as f:
+with open('data/conceptual_prompting/coding_prompts.txt', 'r') as f:
     lines = [i.lstrip().strip() for i in f.readlines()]
 
-with open('data/coding_prompts_clean.txt', 'w') as f:
+with open('data/conceptual_prompting/coding_prompts_clean_v2.txt', 'w') as f:
     for i in lines:
         if i != '':
-            f.write(i + ' Please embed the code within an explicit code block, surrounded by triple-backtick markers.\n')
+            f.write(i + 
+    ' Please embed the code within an explicit code block, surrounded by triple-backtick markers. ' + 
+    'Generate realistic values for any examples, and do not use input() commands. ' + 
+    'Create code that is modular and well-commented.  Then, generate a set of pytest tests that exercise each of the functions, embedded in a separate code block.\n')
